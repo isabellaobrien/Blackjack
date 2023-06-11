217df7f184a29155ae258f6881d26469672ab9ae
isabellaobrien <>
2023-06-11 15:29:27 +0000

    change deal_first_hand to deal_cards and remove deal_again function

--- run.py
+++ run.py
@@ -1,27 +1,39 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import random
+# SCOPE = [
+#     "https://www.googleapis.com/auth/spreadsheets",
+#     "https://www.googleapis.com/auth/drive.file",
+#     "https://www.googleapis.com/auth/drive"
+#     ]
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+# CREDS = Credentials.from_service_account_file('creds.json')
+# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# SHEET = GSPREAD_CLIENT.open('Blackjack1')
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('Blackjack1')
-
-def deal_first_hand():
-
-
-
-def calculate_score():
-
-
-
-def compare():
+def deal_cards():
+    player_cards = []
+    computer_cards = []
+    deck = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K',
+    'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K','A', 2,
+    3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K','A', 2, 3, 4,
+    5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+    
+    random.shuffle(deck)
+    card = deck[0]
+    print(card)
+    deck.remove(card)
 
 
+deal_cards()
 
-def deal_again():
+# def calculate_score():
+
+
+
+# def compare():
+
+
+
+# def deal_again():
