457041c0b1f50eb02d5dd198262c16fc808c7840
isabellaobrien <>
2023-06-14 18:34:13 +0000

    change criteria for blackjack, return 21 not 0

--- run.py
+++ run.py
@@ -13,18 +13,20 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Blackjack1')
 
-def deal_cards():
-    """returns the first card from a shuffled deck of cards.
-     The delt card is then removed from the deck."""
-    deck = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K',
+deck = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K',
     'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K','A', 2,
     3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K','A', 2, 3, 4,
     5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+
+def deal_cards(deck):
+    """returns the first card from a shuffled deck of cards.
+     The delt card is then removed from the deck."""
     
     random.shuffle(deck)
     card = deck[0]
-    return card
     deck.remove(card)
+    return card
+    
 
 def calculate_score(cards):
     """Takes a list of cards and checks if there's a J,Q or K, if 
@@ -44,8 +46,8 @@ def calculate_score(cards):
         else:
             score += card
      
-    if score == 21 and len(cards) == 2:
-        return 0
+    # if score == 21 and len(cards) == 2:
+    #     return 0
 
     return score
 
@@ -60,19 +62,19 @@ def update_sheet(data, worksheet):
 def compare(player_score, computer_score):
     """Compares the user and users score to see who wins"""
     if player_score == computer_score:
-        return "It's a Draw"
-    elif player_score == 0:
-        return f"Blackjack {username} won!"
-    elif computer_score == 0:
-        return f"The computer scored a blackjack, {username} lose."
+        return "It's a Draw\n"
+    elif player_score == 21:
+        return f"Blackjack {username} won!\n"
+    elif computer_score == 21:
+        return f"The computer scored a blackjack, {username} lose.\n"
     elif player_score > 21:
-        return f"{username} went over, {username} lost"
+        return f"{username} went over, {username} lost\n"
     elif computer_score > 21:
-        return f"{username} won, the computer went over!"
+        return f"{username} won, the computer went over!\n"
     elif player_score > computer_score:
-        return f"{username} won!"
+        return f"{username} won!\n"
     else:
-        return f"{username} lost."
+        return f"{username} lost.\n"
 
 
 
@@ -84,14 +86,15 @@ def play():
     two to the computer, it only reveals one of the computers cards. 
     If no one has scored a blackjack or goen over 21 teh game continues, the user can draw another card.
     The computer draws another cards if the score is below 16 and not a blackajack"""
-    
+    # make sure it's really copying original deck, not by referance. print should be 52
+    play_deck = deck
     player_cards = []
     computer_cards = []
     game_over = False
 
     for _ in range(2):
-        player_cards.append(deal_cards())
-        computer_cards.append(deal_cards())
+        player_cards.append(deal_cards(play_deck))
+        computer_cards.append(deal_cards(play_deck))
 
     while not game_over:
         player_score = calculate_score(player_cards)
@@ -99,20 +102,19 @@ def play():
         print(f"These are your current cards: {player_cards}, this is your current score: {player_score}")
         print(f"This is the computer's first card: {computer_cards[0]}")
 
-        if computer_score == 0 or player_score == 0 or player_score > 21:
+        if computer_score == 21 or player_score == 21 or player_score > 21:
             game_over = True
         else:
-            should_continue = input("Do you want to draw another card? type 'y' or 'n':\n ")
+            should_continue = input("Do you want to draw another card? type 'y' or 'n': \n")
             if should_continue == 'y':
-                player_cards.append(deal_cards())
-                print(player_cards)
+                player_cards.append(deal_cards(play_deck))
             elif should_continue == 'n':
                 game_over = True
             else:
                 print("Error, invalid data.Please input valid data. type 'y' or 'n'!!!\n")
 
-    while computer_score != 0 and computer_score < 16:
-        computer_cards.append(deal_cards())
+    while computer_score < 16:
+        computer_cards.append(deal_cards(play_deck))
         computer_score = calculate_score(computer_cards)
     print(f"Your final hand: {player_cards}, your final score: {player_score}")
     print(f"Computer's final hand: {computer_cards}, computer's final score: {computer_score}")
@@ -120,19 +122,19 @@ def play():
     print(outcome)
 
     # this section updates the worksheets and returns the leaderboard 
-    player_final_scores = SHEET.worksheet('player_final_scores')
-    update_sheet([player_score], player_final_scores)
-    computer_final_scores = SHEET.worksheet('computer_final_scores')
-    update_sheet([computer_score], computer_final_scores)
-    winners = SHEET.worksheet('winners')
-    update_sheet([outcome], winners)
-    for i in reversed(range(1, 3)):
-        player = players.row_values(i)
-        player_final_score = player_final_scores.row_values(i)
-        computer_final_score = computer_final_scores.row_values(i)
-        winner = winners.row_values(i)
+    # player_final_scores = SHEET.worksheet('player_final_scores')
+    # update_sheet([player_score], player_final_scores)
+    # computer_final_scores = SHEET.worksheet('computer_final_scores')
+    # update_sheet([computer_score], computer_final_scores)
+    # winners = SHEET.worksheet('winners')
+    # update_sheet([outcome], winners)
+    # for i in reversed(range(1, 3)):
+    #     player = players.row_values(i)
+    #     player_final_score = player_final_scores.row_values(i)
+    #     computer_final_score = computer_final_scores.row_values(i)
+    #     winner = winners.row_values(i)
 
-        print(f"{player[0]} scored: {player_final_score[0]}, computer scored: {computer_final_score[0]} -- {winner[0]}")
+    #     print(f"{player[0]} scored: {player_final_score[0]}, computer scored: {computer_final_score[0]} -- {winner[0]}")
 
 #if the user decides to play another game the name is not saved a second time
 #  but the score is. I've tried updating the worksheet again when the user decides
@@ -144,7 +146,7 @@ while input("Do you want to play a game of blackjack? type 'y' or 'n': \n") == '
     play()
     
 
- 
+
 
 
 
